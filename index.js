ba6749083dff1c737af1e87f63532ffd61977865
Julian Piasecki <julian.piasecki@outlook.com>
2023-01-28 20:59:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const textInput = document.getElementById('text');
+const calculateBtn = document.getElementById('calculate');
+const tableDiv = document.getElementById('table');
+
+let frequencyObject;
+
+const calculateFrequency = () => {
+  if (textInput.value.trim() === '') {
+    tableDiv.innerText = 'Enter text!';
+    return;
+  }
+
+  const frequencyArray = textInput.value.split(' ');
+  frequencyObject = {};
+  for (let word of frequencyArray) {
+    if (word.trim() === '') continue;
+    word = word.replace('.', '');
+    word = word.replace(',', '');
+    word = word.replace('!', '');
+    word = word.replace('?', '');
+    word = word.replace(':', '');
+    word = word.replace(';', '');
+
+    if (word.toLowerCase() in frequencyObject) {
+      frequencyObject[word.toLowerCase()] += 1;
+    } else {
+      frequencyObject[word.toLowerCase()] = 1;
+    }
+  }
+
+  frequencyObject = Object.entries(frequencyObject)
+    .sort((a, b) => b[1] - a[1])
+    .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {});
+
+  createTable();
+};
+
+const createTable = () => {
+  tableDiv.innerHTML = '';
+  const table = document.createElement('table');
+
+  const th1 = document.createElement('th');
+  const th2 = document.createElement('th');
+
+  th1.innerText = 'Word';
+  th2.innerText = 'Frequency';
+
+  table.appendChild(th1);
+  table.appendChild(th2);
+
+  for (const key in frequencyObject) {
+    const tr = document.createElement('tr');
+
+    const td1 = document.createElement('td');
+    td1.innerHTML = key;
+    tr.appendChild(td1);
+
+    const td2 = document.createElement('td');
+    td2.innerHTML = frequencyObject[key];
+    tr.appendChild(td2);
+
+    table.appendChild(tr);
+  }
+
+  tableDiv.appendChild(table);
+};
+
+calculateBtn.onclick = () => calculateFrequency();
